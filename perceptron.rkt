e09bf42f6b449c63c1afb9b12a8ce8170f5a9884
Matthew Eric Bassett <mebassett@gegn.net>
2012-02-05 02:38:17 +0000

    more comments

--- perceptron.rkt
+++ perceptron.rkt
@@ -1,6 +1,26 @@
 #lang racket
+; Matthew Eric Bassett
+; http://mebassett.gegn.net
+; mebassett@gegn.net
+; London, UK
+; 4 Feb 2012
+;
+; Attempt at implementing a perceptron algorithm as per:
+; Tommi Jaakkola, course materials for 6.867 Machine Learning, Fall 2006. 
+; MIT OpenCourseWare (http://ocw.mit.edu/), Massachusetts Institute of Technology. 
+; Downloaded on [3 Feb 2012].
+;
+; The training data is a bunch of 25x25 black/white images.  The perceptron is to
+; identify which images are of a letter A and which ones are not.  I created the 
+; data in GIMP, saved as an HTML table, and used sed to convert it into a list of
+; pixel colors.  I would right a blog about it, but its now nearly 3am.  It's snowing
+; outside and I'd like to go play in the snow, but I think sleep is more important 
+; right now.
+
+;initialize our perceptron.
 (define sigma (build-vector 625 (λ (x) 1)))
 
+;some maths functions
 (define (sign x)
     (if (>= x 0)
         1
@@ -9,9 +29,13 @@
 (define (dot v1 v2)
     (apply + (vector->list (vector-map * v1 v2))))
 
+(define (scalar-mult s v)
+    (vector-map * v (build-vector (vector-length v) (λ (x) s))))
+
 (define (hexstr->number str)
     (string->number (string-append "#x" str)))
 
+;reading the image data files
 (define (img->vector filename)
     (define filelist (with-input-from-file (string->path filename)
                        (λ ()
@@ -27,16 +51,14 @@
         1
         -1))
 
-(define (scalar-mult s v)
-    (vector-map * v (build-vector (vector-length v) (λ (x) s))))
-
-(define (perceptron img)
+;our perceptron stuff
+(define (perceptron-label img)
     (sign (dot sigma img)))
 
 (define (train-perceptron img-name)
-    (define y (filename->label img-name))
+    (define label (filename->label img-name))
     (define vec (img->vector img-name))
-    (cond ((= (perceptron vec) y)
+    (cond ((= (perceptron-label vec) label)
            (display "good!\n"))
           (else 
            (set! sigma (vector-map + sigma (scalar-mult y vec)))
@@ -48,12 +70,19 @@
     (when (regexp-match? #rx"[.]img$" path)
       (train-perceptron (path->string path)))))
 
+;let's see what it thinks before training
 (perceptron (img->vector "images/sample/test1.img"))
 (perceptron (img->vector "images/sample/test2.img"))
 (perceptron (img->vector "images/sample/test3.img"))
 
+;I ran some tests and realized it gets a tad bit better if you train it on
+;the same data several times.  There's one image it just can't get right.
+;Maybe it needs more training data? or maybe this is just a silly way to do
+;this
 (map (λ (x) (train)) (build-list 10 values))
 
 (perceptron (img->vector "images/sample/test1.img"))
 (perceptron (img->vector "images/sample/test2.img"))
 (perceptron (img->vector "images/sample/test3.img"))
+
+;that really wasn't great.  it has a false postive.  oh well!  
